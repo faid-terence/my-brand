df637b9a922ddd831f79670fd6db2c88bbceec91
Faid Terence JABO <faidterence@outlook.com>
2023-02-21 13:11:54 +0200

    Add files via upload
    For creating, deleting and update blogs

---
+++ create.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('form');
+const createPost = async (e) => {
+    e.preventDefault();
+    const doc = {
+        title: form.title.value,
+        body: form.body.value,
+        views: 10090
+    }
+    await fetch('http://localhost:3000/posts', {
+        method: 'POST',
+        body: JSON.stringify(doc),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    window.location.replace('index.html');
+}
+form.addEventListener('submit', createPost);
